ae8891f0ef4ba6b2eea681017a7317a5440fd65e
odbayar94 <odbayar94@gmail.com>
2021-10-02 19:52:47 +0800

    added confi/index.ts

---
+++ src/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+ 
+  port: process.env.PORT,
+  databaseURL: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpiresIn: process.env.JWT_EXPIRESIN,
+
+};
